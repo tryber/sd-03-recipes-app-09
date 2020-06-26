f1b064a2f54a212fad24b96877369d2e0fcc9d03
Gabryel <gabryel.ryba@gmail.com>
2020-06-26 18:08:44 -0300

    Iniciando filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { fetchMealCategories } from '../services/ServiceMeal';
+import { fetchDrinkCategories } from '../services/ServiceDrinks';
+import RecipesContext from '../contexts/RecipesContext';
 
-const Filters = () => (
-  <div>
-    iniciando Filters Component
-  </div>
-);
+const renderFilterButtons = ({ categories }) => {
+  const categoriesArr = [...categories];
+  categoriesArr.map((category, i) => {
+    if (i > 5) return null;
+    return (
+      <button type="button">
+        {category}
+      </button>
+    );
+  });
+};
+
+const Filters = () => {
+  const { mealsCategories, drinksCategories } = useContext(RecipesContext);
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div>
+      {console.log(mealsCategories)}
+    </div>
+  );
+};
 
 export default Filters;
