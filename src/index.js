0dcd432bd575f021d107511a4c79bd318c25fd4d
Gabryel <gabryel.ryba@gmail.com>
2020-06-26 19:25:19 -0300

    Hotfix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import RecipesProvider from './contexts/RecipesContext';
+import RecipesProvider from './contexts/RecipesProvider';
 
 ReactDOM.render(
   <Router>
