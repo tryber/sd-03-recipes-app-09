7c7f2829247192bbb4557e1d85de8c80ee49b6bf
Gabryel <gabryel.ryba@gmail.com>
2020-06-29 11:49:54 -0300

    Fixed ServiceDrinks functions

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,30 +1,40 @@
 import React, { useEffect, useContext } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { fetchMealCategories } from '../services/ServiceMeal';
 import { fetchDrinkCategories } from '../services/ServiceDrinks';
 import RecipesContext from '../contexts/RecipesContext';
 
-const renderFilterButtons = ({ categories }) => {
-  const categoriesArr = [...categories];
-  categoriesArr.map((category, i) => {
+const renderFilterButtons = (categories) => {
+  if (categories.length === 0) return null;
+  return categories.map((category, i) => {
     if (i > 5) return null;
     return (
       <button type="button">
-        {category}
+        {category.strCategory}
       </button>
     );
   });
 };
 
 const Filters = () => {
-  const { mealsCategories, drinksCategories } = useContext(RecipesContext);
-  const { match: { url } } = useRouteMatch();
+  const {
+    mealsCategories,
+    setMealsCategories,
+    drinksCategories,
+    setDrinksCategories,
+  } = useContext(RecipesContext);
+  const { pathname } = useLocation();
   useEffect(() => {
+    if (pathname === '/bebidas') {
+      fetchDrinkCategories()
+        .then((response) => console.log(response));
+    }
+    fetchMealCategories().then((response) => setMealsCategories(response.meals));
   }, []);
 
   return (
     <div>
-      {url}
+      {console.log(drinksCategories)}
     </div>
   );
 };
