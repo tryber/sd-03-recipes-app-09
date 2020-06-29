ca990f8bb3a69ddcbffebbd1bffdeeba1f8b64d1
Gabryel <gabryel.ryba@gmail.com>
2020-06-29 18:35:15 -0300

    Filters component doesnt fetch for data anymore

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
-import { fetchMealCategories, fetchMealsByCategory, fetchMeals } from '../services/ServiceMeal';
-import { fetchDrinkCategories, fetchDrinksByCategory, fetchDrinks } from '../services/ServiceDrinks';
+import { fetchMealCategories } from '../services/ServiceMeal';
+import { fetchDrinkCategories } from '../services/ServiceDrinks';
 import RecipesContext from '../contexts/RecipesContext';
 
 const Filters = () => {
   const {
-    setMealsData,
-    setDrinksData,
     mealsCategories,
     setMealsCategories,
     drinkCategories,
@@ -26,46 +24,19 @@ const Filters = () => {
       .then((response) => setMealsCategories(response.meals));
   }, []);
 
-  const disableFilters = () => {
-    if (pathname === '/bebidas') {
-      return fetchDrinks()
-        .then((result) => setDrinksData(result.drinks));
-    }
-    return fetchMeals()
-      .then((result) => setMealsData(result.meals));
-  };
-
-  const filterDataByCategory = (category) => {
-    if (pathname === '/bebidas') {
-      if (category === selectedCategory) {
-        setSelectedCategory('All');
-        return disableFilters();
-      }
-      return fetchDrinksByCategory(category)
-        .then((result) => setDrinksData(result.drinks))
-        .then(() => setSelectedCategory(category));
-    }
-    if (category === selectedCategory) {
-      setSelectedCategory('All');
-      return disableFilters();
-    }
-    return fetchMealsByCategory(category)
-      .then((result) => setMealsData(result.meals))
-      .then(() => setSelectedCategory(category));
-  };
-
   const renderFilterButtons = (categories) => {
     if (categories.length === 0) return null;
     return (
       <div>
-        <button onClick={disableFilters} type="button">All</button>
+        <button onClick={(e) => setSelectedCategory(e.target.value)} type="button">All</button>
         {categories.map((category, i) => {
           if (i > 4) return null;
           return (
             <button
               data-testid={`${category.strCategory}-category-filter`}
               type="button"
-              onClick={() => filterDataByCategory(category.strCategory)}
+              value={category.strCategory}
+              onClick={(e) => setSelectedCategory(e.target.value)}
             >
               {category.strCategory}
             </button>
@@ -79,6 +50,7 @@ const Filters = () => {
     <div>
       {renderFilterButtons(pathname === '/comidas'
         ? mealsCategories : drinkCategories)}
+      {console.log(selectedCategory)}
     </div>
   );
 };
