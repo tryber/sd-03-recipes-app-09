59c3c30358e95e7f402fa0729e182887fcd3078b
Gabryel <gabryel.ryba@gmail.com>
2020-06-29 12:31:51 -0300

    Filter buttons now fecth by category onClick

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,40 +1,65 @@
 import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
-import { fetchMealCategories } from '../services/ServiceMeal';
-import { fetchDrinkCategories } from '../services/ServiceDrinks';
+import { fetchMealCategories, fetchMealsByCategory } from '../services/ServiceMeal';
+import { fetchDrinkCategories, fetchDrinksByCategory } from '../services/ServiceDrinks';
 import RecipesContext from '../contexts/RecipesContext';
-
-const renderFilterButtons = (categories) => {
-  if (categories.length === 0) return null;
-  return categories.map((category, i) => {
-    if (i > 5) return null;
-    return (
-      <button type="button">
-        {category.strCategory}
-      </button>
-    );
-  });
-};
 
 const Filters = () => {
   const {
+    mealsData,
+    setMealsData,
+    drinksData,
+    setDrinksData,
     mealsCategories,
     setMealsCategories,
     drinkCategories,
     setDrinksCategories,
   } = useContext(RecipesContext);
   const { pathname } = useLocation();
+
   useEffect(() => {
     if (pathname === '/bebidas') {
       fetchDrinkCategories()
         .then((response) => setDrinksCategories(response.drinks));
     }
-    fetchMealCategories().then((response) => setMealsCategories(response.meals));
+    fetchMealCategories()
+      .then((response) => setMealsCategories(response.meals));
   }, []);
+
+  const filterDataByCategory = (category) => {
+    if (pathname === '/bebidas') {
+      return fetchDrinksByCategory(category)
+        .then((result) => setMealsData(result.drinks));
+    }
+    return fetchMealsByCategory(category)
+      .then((result) => setDrinksData(result.meals));
+  };
+
+  const renderFilterButtons = (categories) => {
+    if (categories.length === 0) return null;
+    return (
+      <div>
+        <button type="button">All</button>
+        {categories.map((category, i) => {
+          if (i > 5) return null;
+          return (
+            <button
+              data-testid={`${category.strCategory}-category-filter`}
+              type="button"
+              onClick={() => filterDataByCategory(category.strCategory)}
+            >
+              {category.strCategory}
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
 
   return (
     <div>
-      {renderFilterButtons(pathname === '/comidas' ? mealsCategories : drinkCategories)}
+      {renderFilterButtons(pathname === '/comidas'
+        ? mealsCategories : drinkCategories)}
     </div>
   );
 };
