f55cdaa23aa12feec47734ba809d130229c81c0c
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-06-29 17:47:05 -0300

    Explore Page

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExplorePage() {
   return (
     <div>
       <Header />
-      <p>tela de explorar</p>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
+      <Footer />
     </div>
   );
 }
