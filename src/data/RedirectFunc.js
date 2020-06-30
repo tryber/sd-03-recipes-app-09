84c5eb2701f56ada842e5d18374b953e1f32ac3c
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-06-30 20:54:04 -0300

    Correção da RedirectFunc

--- src/data/RedirectFunc.js
+++ src/data/RedirectFunc.js
@@ -1,29 +1,24 @@
 import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Redirect, useLocation } from 'react-router-dom';
 import RecipesContext from '../contexts/RecipesContext';
+import { fetchDrinksById } from '../services/ServiceDrinks';
+import { fetchMealsById } from '../services/ServiceMeals';
 
-function RedirectFunc() {
-  const { mealsData, drinksData } = useContext(RecipesContext);
-  if (mealsData && mealsData.length === 1) {
-    return (
-      <Redirect
-        to={{
-          pathname: `/comidas/${mealsData[0].idMeal}`,
-          state: { data: mealsData },
-        }}
-      />
-    );
-  } else if (drinksData && drinksData.length === 1) {
-    console.log('drink');
-    return (
-      <Redirect
-        to={{
-          pathname: `/bebidas/${drinksData[0].idDrink}`,
-          state: { data: drinksData },
-        }}
-      />
-    );
-  }
+function RedirectFunc({ id }) {
+  const { setMealsData, setDrinksData } = useContext(RecipesContext);
+  const { pathname } = useLocation();
+
+  if (pathname.includes('/bebidas')) {
+    fetchDrinksById(id).then(({ drinks }) => setDrinksData(drinks));
+    return <Redirect to={`/bebidas/${id}`} />
+  } 
+  fetchMealsById(id).then(({ meals }) => setMealsData(meals));
+  return <Redirect to={`/comidas/${id}`} />
+}
+
+RedirectFunc.propTypes = {
+  id: PropTypes.number.isRequired,
 }
 
 export default RedirectFunc;
