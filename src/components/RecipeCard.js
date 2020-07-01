02e64756458eba3b63342712c02e4e6639d50a2e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-06-30 21:37:09 -0300

    CC Issues/3

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import RedirectFunc from '../data/RedirectFunc';
 
-function RecipeCard({ imgSrc, name, id, recipe }) {
+function RecipeCard({ imgSrc, name, id }) {
+  const onClick = () => (
+    <RedirectFunc id={id} />
+  )
+
   return (
-    <div>
+    <div onClick={onClick}>
       <img className="img" src={imgSrc} alt={name} />
       <p>{name}</p>
     </div>
   );
+}
+
+RecipeCard.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 }
 
 export default RecipeCard;
