6045fac5f0e51946ec6dad78bae9a0a76b9957f8
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-06-30 21:40:23 -0300

    CC Issues/4

--- src/components/GetRecipesCards.js
+++ src/components/GetRecipesCards.js
@@ -7,28 +7,29 @@ function GetRecipesCards() {
   const { mealsData, drinksData } = useContext(RecipesContext);
   const { pathname } = useLocation();
   if (pathname === '/comidas' && mealsData) {
-    return mealsData.map((recipe, index) =>
-      index < 12 ? (
+    return mealsData.map((recipe, index) => {
+      return index < 12 ? (
         <RecipeCard
           key={recipe.idMeal}
           imgSrc={recipe.strMealThumb}
           name={recipe.strMeal}
           id={recipe.idMeal}
         />
-      ) : undefined
-    );
+      ) : undefined;
+    });
   } else if (pathname === '/bebidas' && drinksData) {
-    return drinksData.map((recipe, index) =>
-      index < 12 ? (
+    return drinksData.map((recipe, index) => {
+      return index < 12 ? (
         <RecipeCard
           key={recipe.idDrink}
           imgSrc={recipe.strDrinkThumb}
           name={recipe.strDrink}
           id={recipe.idDrink}
         />
-      ) : undefined
-    );
-  } return <div />;
+      ) : undefined;
+    });
+  }
+  return <div />;
 }
 
 export default GetRecipesCards;
