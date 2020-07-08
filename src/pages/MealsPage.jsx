46bd3274c4fbe1a130bfbf5db80e9bcc2946e827
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-07-08 17:43:34 -0300

    MealsPage

--- src/pages/MealsPage.jsx
+++ src/pages/MealsPage.jsx
@@ -9,5 +9,47 @@ const MealsPage = () => (
     <Footer />
   </div>
 );
+import React, { useContext, useEffect } from 'react';
+import RecipesContext from '../contexts/RecipesContext';
+import { fetchMeals, fetchMealsByCategory } from '../services/ServiceMeals';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import GetRecipesCards from '../components/GetRecipesCards';
+import RedirectFunc from '../data/RedirectFunc';
+import Filters from '../components/Filters';
+
+const MealsPage = () => {
+  const {
+    mealsData,
+    setMealsData,
+    toggleSearchBar,
+    selectedCategory,
+  } = useContext(RecipesContext);
+
+  useEffect(() => {
+    if (selectedCategory === 'All') {
+      fetchMeals().then(({ meals }) => setMealsData(meals), setMealsData([]));
+    } else {
+      fetchMealsByCategory(selectedCategory).then(
+        ({ meals }) => setMealsData(meals),
+        setMealsData([]),
+      );
+    }
+  }, [selectedCategory]);
+
+  if (toggleSearchBar && mealsData && mealsData.length === 1) {
+    return <RedirectFunc id={mealsData[0].idMeal} />;
+  } else if (toggleSearchBar && mealsData === null) {
+    alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+  }
+  return (
+    <div>
+      <Header />
+      <Filters />
+      <GetRecipesCards />
+      <Footer />
+    </div>
+  );
+};
 
 export default MealsPage;
