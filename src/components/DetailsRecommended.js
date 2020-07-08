7a83f66ffe96ea12fefcf36c92f2fd435749a639
Gabryel <gabryel.ryba@gmail.com>
2020-07-07 23:06:15 -0300

    details recommended hotfix

--- src/components/DetailsRecommended.js
+++ src/components/DetailsRecommended.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { fetchMeals } from '../services/ServiceMeal';
+import { fetchMeals } from '../services/ServiceMeals';
 import { fetchDrinks } from '../services/ServiceDrinks';
 import { sortMealData, sortDrinkData } from '../data/helpers/sortData';
 import RecommendationCard from './RecommendationCard';
