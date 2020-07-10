b2ee79dfd03fa1a830e9b73f499b9c07685b8955
Gabryel <gabryel.ryba@gmail.com>
2020-07-10 12:10:12 -0300

    reusable share button

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -4,19 +4,26 @@ import shareIcon from '../images/shareIcon.svg';
 
 const copyLinkToClipboard = (pathname) => {
   navigator.clipboard.writeText(`http://localhost:3000${pathname}`);
-  const titleWrapper = document.getElementById('title-wrapper');
+  const button = document.getElementById('share-button');
+  const buttonWrapper = document.getElementById('share-button-wrapper');
   const copyAlert = document.createElement('span');
   copyAlert.innerHTML = 'Link copiado!';
-  titleWrapper.appendChild(copyAlert);
-  setTimeout(() => titleWrapper.removeChild(copyAlert), 1000);
+  buttonWrapper.removeChild(button);
+  buttonWrapper.appendChild(copyAlert);
+  setTimeout(() => {
+    buttonWrapper.removeChild(copyAlert);
+    buttonWrapper.appendChild(button);
+  }, 1000);
 };
 
 const ShareButton = () => {
   const { pathname } = useLocation();
   return (
-    <button data-testid="share-btn" type="button" onClick={() => copyLinkToClipboard(pathname)}>
-      <img alt="share-icon" src={shareIcon} />
-    </button>
+    <div id="share-button-wrapper">
+      <button id="share-button" data-testid="share-btn" type="button" onClick={() => copyLinkToClipboard(pathname)}>
+        <img alt="share-icon" src={shareIcon} />
+      </button>
+    </div>
   );
 };
 
